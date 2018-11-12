74c2d30a8619fc880bded883e23149a4bc8d96dc
Hoang Minh Ho <panzer3553@gmail.com>
2018-11-12 11:11:41 +0700

    Always search lower case

--- dist/lib/utils.js
+++ dist/lib/utils.js
@@ -25,21 +25,21 @@ var _require2 = require('./LogicalIterator'),
 
 
 var sliceRelation = function sliceRelation(relatedProperty) {
-  var delimiter = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : '.';
+	var delimiter = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : '.';
 
-  var split = relatedProperty.split('.');
-  var propertyName = split[split.length - 1];
-  var relationName = split.slice(0, split.length - 1).join(delimiter);
+	var split = relatedProperty.split('.');
+	var propertyName = split[split.length - 1];
+	var relationName = split.slice(0, split.length - 1).join(delimiter);
 
-  // Nested relations need to be in the format a:b:c.name
-  // https://github.com/Vincit/objection.js/issues/363
-  var fullyQualifiedProperty = relationName ? relationName.replace(/\./g, ':') + '.' + propertyName : propertyName;
+	// Nested relations need to be in the format a:b:c.name
+	// https://github.com/Vincit/objection.js/issues/363
+	var fullyQualifiedProperty = relationName ? relationName.replace(/\./g, ':') + '.' + propertyName : propertyName;
 
-  return {
-    propertyName: propertyName,
-    relationName: relationName,
-    fullyQualifiedProperty: fullyQualifiedProperty
-  };
+	return {
+		propertyName: propertyName,
+		relationName: relationName,
+		fullyQualifiedProperty: fullyQualifiedProperty
+	};
 };
 module.exports.sliceRelation = sliceRelation;
 
@@ -49,123 +49,123 @@ module.exports.sliceRelation = sliceRelation;
  * @param {Object} options.operators
  */
 module.exports.Operations = function (options) {
-  var defaultOperators = {
-    $like: function $like(property, operand, builder) {
-      return builder.where(property, 'like', '%' + operand + '%');
-    },
-    $ilike: function $ilike(property, operand, builder) {
-      return builder.where(property, 'ilike', '%' + operand + '%');
-    },
-    $lt: function $lt(property, operand, builder) {
-      return builder.where(property, '<', operand);
-    },
-    $gt: function $gt(property, operand, builder) {
-      return builder.where(property, '>', operand);
-    },
-    $lte: function $lte(property, operand, builder) {
-      return builder.where(property, '<=', operand);
-    },
-    $gte: function $gte(property, operand, builder) {
-      return builder.where(property, '>=', operand);
-    },
-    $equals: function $equals(property, operand, builder) {
-      return builder.where(property, operand);
-    },
-    '=': function _(property, operand, builder) {
-      return builder.where(property, operand);
-    },
-    $in: function $in(property, operand, builder) {
-      return builder.where(property, 'in', operand);
-    },
-    $exists: function $exists(property, operand, builder) {
-      return operand ? builder.whereNotNull(property) : builder.whereNull(property);
-    },
-    $all: function $all(property, operand, builder) {
-      return builder.whereJsonSupersetOf(property, operand);
-    },
-    /**
-     * @param {String} property
-     * @param {Array} items Must be an array of objects/values
-     * @param {QueryBuilder} builder
-     */
-    $or: function $or(property, items, builder) {
-      var onExit = function onExit(operator, value, subQueryBuilder) {
-        var operationHandler = allOperators[operator];
-        operationHandler(property, value, subQueryBuilder);
-      };
-      var onLiteral = function onLiteral(value, subQueryBuilder) {
-        onExit('$equals', value, subQueryBuilder);
-      };
-
-      // Iterate the logical expression until it hits an operation e.g. $gte
-      var iterateLogical = iterateLogicalExpression({
-        onExit: onExit,
-        onLiteral: onLiteral
-      });
-
-      // Wrap within another builder context to prevent end-of-expression errors
-      // TODO: Investigate the consequences of not using this wrapper
-      return builder.where(function (subQueryBuilder) {
-        iterateLogical({
-          $or: items
-        }, subQueryBuilder, true);
-      });
-    },
-    $and: function $and(property, items, builder) {
-      var onExit = function onExit(operator, value, subQueryBuilder) {
-        var operationHandler = allOperators[operator];
-        operationHandler(property, value, subQueryBuilder);
-      };
-      var onLiteral = function onLiteral(value, subQueryBuilder) {
-        onExit('$equals', value, subQueryBuilder);
-      };
-
-      // Iterate the logical expression until it hits an operation e.g. $gte
-      var iterateLogical = iterateLogicalExpression({
-        onExit: onExit,
-        onLiteral: onLiteral
-      });
-
-      // Wrap within another builder context to prevent end-of-expression errors
-      return builder.where(function (subQueryBuilder) {
-        iterateLogical({
-          $and: items
-        }, subQueryBuilder, false);
-      });
-    }
-  };
-  var operators = options.operators;
-
-  // Custom operators take override default operators
-
-  var allOperators = Object.assign({}, defaultOperators, operators);
-
-  /**
-   * Apply a subset of operators on a single property
-   * @param {String} propertyName
-   * @param {Object} expression
+	var defaultOperators = {
+		$like: function $like(property, operand, builder) {
+			return builder.where(property, 'ilike', '%' + operand + '%');
+		},
+		$ilike: function $ilike(property, operand, builder) {
+			return builder.where(property, 'ilike', '%' + operand + '%');
+		},
+		$lt: function $lt(property, operand, builder) {
+			return builder.where(property, '<', operand);
+		},
+		$gt: function $gt(property, operand, builder) {
+			return builder.where(property, '>', operand);
+		},
+		$lte: function $lte(property, operand, builder) {
+			return builder.where(property, '<=', operand);
+		},
+		$gte: function $gte(property, operand, builder) {
+			return builder.where(property, '>=', operand);
+		},
+		$equals: function $equals(property, operand, builder) {
+			return builder.where(property, operand);
+		},
+		'=': function _(property, operand, builder) {
+			return builder.where(property, operand);
+		},
+		$in: function $in(property, operand, builder) {
+			return builder.where(property, 'in', operand);
+		},
+		$exists: function $exists(property, operand, builder) {
+			return operand ? builder.whereNotNull(property) : builder.whereNull(property);
+		},
+		$all: function $all(property, operand, builder) {
+			return builder.whereJsonSupersetOf(property, operand);
+		},
+		/**
+   * @param {String} property
+   * @param {Array} items Must be an array of objects/values
    * @param {QueryBuilder} builder
    */
-  var applyPropertyExpression = function applyPropertyExpression(propertyName, expression, builder) {
-    debug('Handling property[' + propertyName + '] expression[' + JSON.stringify(expression) + ']');
+		$or: function $or(property, items, builder) {
+			var onExit = function onExit(operator, value, subQueryBuilder) {
+				var operationHandler = allOperators[operator];
+				operationHandler(property, value, subQueryBuilder);
+			};
+			var onLiteral = function onLiteral(value, subQueryBuilder) {
+				onExit('$equals', value, subQueryBuilder);
+			};
 
-    // If the rhs is a primitive, assume equality
-    if ((typeof expression === 'undefined' ? 'undefined' : _typeof(expression)) !== 'object') return allOperators.$equals(propertyName, expression, builder);
+			// Iterate the logical expression until it hits an operation e.g. $gte
+			var iterateLogical = iterateLogicalExpression({
+				onExit: onExit,
+				onLiteral: onLiteral
+			});
 
-    for (var lhs in expression) {
-      var operationHandler = allOperators[lhs];
-      var rhs = expression[lhs];
+			// Wrap within another builder context to prevent end-of-expression errors
+			// TODO: Investigate the consequences of not using this wrapper
+			return builder.where(function (subQueryBuilder) {
+				iterateLogical({
+					$or: items
+				}, subQueryBuilder, true);
+			});
+		},
+		$and: function $and(property, items, builder) {
+			var onExit = function onExit(operator, value, subQueryBuilder) {
+				var operationHandler = allOperators[operator];
+				operationHandler(property, value, subQueryBuilder);
+			};
+			var onLiteral = function onLiteral(value, subQueryBuilder) {
+				onExit('$equals', value, subQueryBuilder);
+			};
 
-      if (!operationHandler) {
-        debug('The operator [' + lhs + '] does not exist, skipping');
-        continue;
-      }
+			// Iterate the logical expression until it hits an operation e.g. $gte
+			var iterateLogical = iterateLogicalExpression({
+				onExit: onExit,
+				onLiteral: onLiteral
+			});
 
-      operationHandler(propertyName, rhs, builder);
-    }
-  };
+			// Wrap within another builder context to prevent end-of-expression errors
+			return builder.where(function (subQueryBuilder) {
+				iterateLogical({
+					$and: items
+				}, subQueryBuilder, false);
+			});
+		}
+	};
+	var operators = options.operators;
 
-  return {
-    applyPropertyExpression: applyPropertyExpression
-  };
+	// Custom operators take override default operators
+
+	var allOperators = Object.assign({}, defaultOperators, operators);
+
+	/**
+  * Apply a subset of operators on a single property
+  * @param {String} propertyName
+  * @param {Object} expression
+  * @param {QueryBuilder} builder
+  */
+	var applyPropertyExpression = function applyPropertyExpression(propertyName, expression, builder) {
+		debug('Handling property[' + propertyName + '] expression[' + JSON.stringify(expression) + ']');
+
+		// If the rhs is a primitive, assume equality
+		if ((typeof expression === 'undefined' ? 'undefined' : _typeof(expression)) !== 'object') return allOperators.$equals(propertyName, expression, builder);
+
+		for (var lhs in expression) {
+			var operationHandler = allOperators[lhs];
+			var rhs = expression[lhs];
+
+			if (!operationHandler) {
+				debug('The operator [' + lhs + '] does not exist, skipping');
+				continue;
+			}
+
+			operationHandler(propertyName, rhs, builder);
+		}
+	};
+
+	return {
+		applyPropertyExpression: applyPropertyExpression
+	};
 };
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -5,12 +5,8 @@
  */
 
 const _ = require('lodash');
-const {
-  debug
-} = require('../config');
-const {
-  iterateLogicalExpression
-} = require('./LogicalIterator');
+const { debug } = require('../config');
+const { iterateLogicalExpression } = require('./LogicalIterator');
 
 /**
  * For a property "a.b.c", slice it into relationName: "a.b", "propertyName": "c" and
@@ -19,21 +15,21 @@ const {
  * @param {String} delimiter A delimeter to use on the relation e.g. "." or ":"
  */
 const sliceRelation = (relatedProperty, delimiter = '.') => {
-  const split = relatedProperty.split('.');
-  const propertyName = split[split.length - 1];
-  const relationName = split.slice(0, split.length - 1).join(delimiter);
+	const split = relatedProperty.split('.');
+	const propertyName = split[split.length - 1];
+	const relationName = split.slice(0, split.length - 1).join(delimiter);
 
-  // Nested relations need to be in the format a:b:c.name
-  // https://github.com/Vincit/objection.js/issues/363
-  const fullyQualifiedProperty = relationName ?
-    `${relationName.replace(/\./g, ':')}.${propertyName}` :
-    propertyName;
+	// Nested relations need to be in the format a:b:c.name
+	// https://github.com/Vincit/objection.js/issues/363
+	const fullyQualifiedProperty = relationName
+		? `${relationName.replace(/\./g, ':')}.${propertyName}`
+		: propertyName;
 
-  return {
-    propertyName,
-    relationName,
-    fullyQualifiedProperty
-  };
+	return {
+		propertyName,
+		relationName,
+		fullyQualifiedProperty
+	};
 };
 module.exports.sliceRelation = sliceRelation;
 
@@ -42,114 +38,120 @@ module.exports.sliceRelation = sliceRelation;
  * If options.operators is specified
  * @param {Object} options.operators
  */
-module.exports.Operations = function (options) {
-  const defaultOperators = {
-    $like: (property, operand, builder) =>
-      builder.where(property, 'like', `%${operand}%`),
-    $ilike: (property, operand, builder) =>
-      builder.where(property, 'ilike', `%${operand}%`),
-    $lt: (property, operand, builder) => builder.where(property, '<', operand),
-    $gt: (property, operand, builder) => builder.where(property, '>', operand),
-    $lte: (property, operand, builder) =>
-      builder.where(property, '<=', operand),
-    $gte: (property, operand, builder) =>
-      builder.where(property, '>=', operand),
-    $equals: (property, operand, builder) => builder.where(property, operand),
-    '=': (property, operand, builder) => builder.where(property, operand),
-    $in: (property, operand, builder) => builder.where(property, 'in', operand),
-    $exists: (property, operand, builder) =>
-      operand ? builder.whereNotNull(property) : builder.whereNull(property),
-    $all: (property, operand, builder) =>
-      builder.whereJsonSupersetOf(property, operand),
-    /**
-     * @param {String} property
-     * @param {Array} items Must be an array of objects/values
-     * @param {QueryBuilder} builder
-     */
-    $or: (property, items, builder) => {
-      const onExit = function (operator, value, subQueryBuilder) {
-        const operationHandler = allOperators[operator];
-        operationHandler(property, value, subQueryBuilder);
-      };
-      const onLiteral = function (value, subQueryBuilder) {
-        onExit('$equals', value, subQueryBuilder);
-      };
+module.exports.Operations = function(options) {
+	const defaultOperators = {
+		$like: (property, operand, builder) =>
+			builder.where(property, 'ilike', `%${operand}%`),
+		$ilike: (property, operand, builder) =>
+			builder.where(property, 'ilike', `%${operand}%`),
+		$lt: (property, operand, builder) => builder.where(property, '<', operand),
+		$gt: (property, operand, builder) => builder.where(property, '>', operand),
+		$lte: (property, operand, builder) =>
+			builder.where(property, '<=', operand),
+		$gte: (property, operand, builder) =>
+			builder.where(property, '>=', operand),
+		$equals: (property, operand, builder) => builder.where(property, operand),
+		'=': (property, operand, builder) => builder.where(property, operand),
+		$in: (property, operand, builder) => builder.where(property, 'in', operand),
+		$exists: (property, operand, builder) =>
+			operand ? builder.whereNotNull(property) : builder.whereNull(property),
+		$all: (property, operand, builder) =>
+			builder.whereJsonSupersetOf(property, operand),
+		/**
+		 * @param {String} property
+		 * @param {Array} items Must be an array of objects/values
+		 * @param {QueryBuilder} builder
+		 */
+		$or: (property, items, builder) => {
+			const onExit = function(operator, value, subQueryBuilder) {
+				const operationHandler = allOperators[operator];
+				operationHandler(property, value, subQueryBuilder);
+			};
+			const onLiteral = function(value, subQueryBuilder) {
+				onExit('$equals', value, subQueryBuilder);
+			};
 
-      // Iterate the logical expression until it hits an operation e.g. $gte
-      const iterateLogical = iterateLogicalExpression({
-        onExit,
-        onLiteral
-      });
+			// Iterate the logical expression until it hits an operation e.g. $gte
+			const iterateLogical = iterateLogicalExpression({
+				onExit,
+				onLiteral
+			});
 
-      // Wrap within another builder context to prevent end-of-expression errors
-      // TODO: Investigate the consequences of not using this wrapper
-      return builder.where(subQueryBuilder => {
-        iterateLogical({
-          $or: items
-        }, subQueryBuilder, true);
-      });
-    },
-    $and: (property, items, builder) => {
-      const onExit = function (operator, value, subQueryBuilder) {
-        const operationHandler = allOperators[operator];
-        operationHandler(property, value, subQueryBuilder);
-      };
-      const onLiteral = function (value, subQueryBuilder) {
-        onExit('$equals', value, subQueryBuilder);
-      };
+			// Wrap within another builder context to prevent end-of-expression errors
+			// TODO: Investigate the consequences of not using this wrapper
+			return builder.where((subQueryBuilder) => {
+				iterateLogical(
+					{
+						$or: items
+					},
+					subQueryBuilder,
+					true
+				);
+			});
+		},
+		$and: (property, items, builder) => {
+			const onExit = function(operator, value, subQueryBuilder) {
+				const operationHandler = allOperators[operator];
+				operationHandler(property, value, subQueryBuilder);
+			};
+			const onLiteral = function(value, subQueryBuilder) {
+				onExit('$equals', value, subQueryBuilder);
+			};
 
-      // Iterate the logical expression until it hits an operation e.g. $gte
-      const iterateLogical = iterateLogicalExpression({
-        onExit,
-        onLiteral
-      });
+			// Iterate the logical expression until it hits an operation e.g. $gte
+			const iterateLogical = iterateLogicalExpression({
+				onExit,
+				onLiteral
+			});
 
-      // Wrap within another builder context to prevent end-of-expression errors
-      return builder.where(subQueryBuilder => {
-        iterateLogical({
-          $and: items
-        }, subQueryBuilder, false);
-      });
-    }
-  };
-  const {
-    operators
-  } = options;
+			// Wrap within another builder context to prevent end-of-expression errors
+			return builder.where((subQueryBuilder) => {
+				iterateLogical(
+					{
+						$and: items
+					},
+					subQueryBuilder,
+					false
+				);
+			});
+		}
+	};
+	const { operators } = options;
 
-  // Custom operators take override default operators
-  const allOperators = Object.assign({}, defaultOperators, operators);
+	// Custom operators take override default operators
+	const allOperators = Object.assign({}, defaultOperators, operators);
 
-  /**
-   * Apply a subset of operators on a single property
-   * @param {String} propertyName
-   * @param {Object} expression
-   * @param {QueryBuilder} builder
-   */
-  const applyPropertyExpression = function (propertyName, expression, builder) {
-    debug(
-      `Handling property[${propertyName}] expression[${JSON.stringify(
-        expression
-      )}]`
-    );
+	/**
+	 * Apply a subset of operators on a single property
+	 * @param {String} propertyName
+	 * @param {Object} expression
+	 * @param {QueryBuilder} builder
+	 */
+	const applyPropertyExpression = function(propertyName, expression, builder) {
+		debug(
+			`Handling property[${propertyName}] expression[${JSON.stringify(
+				expression
+			)}]`
+		);
 
-    // If the rhs is a primitive, assume equality
-    if (typeof expression !== 'object')
-      return allOperators.$equals(propertyName, expression, builder);
+		// If the rhs is a primitive, assume equality
+		if (typeof expression !== 'object')
+			return allOperators.$equals(propertyName, expression, builder);
 
-    for (let lhs in expression) {
-      const operationHandler = allOperators[lhs];
-      const rhs = expression[lhs];
+		for (let lhs in expression) {
+			const operationHandler = allOperators[lhs];
+			const rhs = expression[lhs];
 
-      if (!operationHandler) {
-        debug(`The operator [${lhs}] does not exist, skipping`);
-        continue;
-      }
+			if (!operationHandler) {
+				debug(`The operator [${lhs}] does not exist, skipping`);
+				continue;
+			}
 
-      operationHandler(propertyName, rhs, builder);
-    }
-  };
+			operationHandler(propertyName, rhs, builder);
+		}
+	};
 
-  return {
-    applyPropertyExpression
-  };
+	return {
+		applyPropertyExpression
+	};
 };
